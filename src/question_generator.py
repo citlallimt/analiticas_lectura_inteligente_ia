0acbac44f1edec8823810156ff7b2fe0e11732f2
Citlalli_MT <citlallileilanimejiatorrijos@gmail.com>
2026-07-02 17:30:20 -0600

    Update question_generator.py

--- src/question_generator.py
+++ src/question_generator.py
@@ -1,4 +1,9 @@
 """
-Módulo encargado de generar preguntas automáticamente
-a partir del contenido del documento utilizando T5.
+Módulo encargado de la generación automática de preguntas
+a partir del contenido del documento utilizando el modelo T5.
+
+Funciones:
+- Carga del modelo T5.
+- Generación de preguntas.
+- Filtrado de preguntas repetidas.
 """
