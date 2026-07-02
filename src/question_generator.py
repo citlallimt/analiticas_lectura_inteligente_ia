6ab1bfaba072570c9bbde63ec5d40ab4088ea43e
Citlalli_MT <citlallileilanimejiatorrijos@gmail.com>
2026-07-02 17:33:53 -0600

    Update question_generator.py

--- src/question_generator.py
+++ src/question_generator.py
@@ -4,6 +4,12 @@ a partir del contenido del documento utilizando el modelo T5.
 
 Funciones:
 - Carga del modelo T5.
-- Generación de preguntas.
-- Filtrado de preguntas repetidas.
+- Generación automática de preguntas.
+- Eliminación de preguntas repetidas.
 """
+
+from transformers import T5ForConditionalGeneration
+from transformers import T5TokenizerFast
+from thefuzz import fuzz
+import re
+import math
