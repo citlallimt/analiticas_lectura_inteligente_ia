ac71f0929fe9e7ee7093514b8da4917f8570d024
Citlalli_MT <citlallileilanimejiatorrijos@gmail.com>
2026-07-02 15:50:45 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-Crear archivo principal main.py
+"""
+Archivo principal del proyecto.
+
+Desde este módulo se inicia el flujo completo del sistema de
+Analíticas de Lectura Inteligente.
+"""
+
+if __name__ == "__main__":
+    print("Analíticas de Lectura Inteligente")
