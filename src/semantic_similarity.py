8b8793950235ed671235bbf560f237e156a30680
Citlalli_MT <citlallileilanimejiatorrijos@gmail.com>
2026-07-02 17:36:42 -0600

    Update semantic_similarity.py

--- src/semantic_similarity.py
+++ src/semantic_similarity.py
@@ -1,5 +1,11 @@
 """
-Módulo encargado de calcular la similitud semántica
-entre la pregunta y la respuesta utilizando
-Sentence Transformers.
+Módulo encargado de evaluar la similitud semántica
+entre preguntas y respuestas mediante Sentence Transformers.
+
+Funciones:
+- Generación de embeddings.
+- Cálculo de similitud semántica.
 """
+
+from sentence_transformers import SentenceTransformer
+from sentence_transformers import util
