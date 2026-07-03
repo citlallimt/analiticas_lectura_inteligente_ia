403ccec2d2fbbc9ceed4c6b9a8c69390413329f7
Citlalli_MT <citlallileilanimejiatorrijos@gmail.com>
2026-07-02 18:04:57 -0600

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -16,20 +16,55 @@ import warnings
 from language_detector import detect_text_language
 
 
-def load_text(file_path):
+def load_text(file_path: str) -> str:
     """
     Lee un archivo de texto.
-
-    Parámetros
-    ----------
-    file_path : str
-        Ruta del archivo de texto.
-
-    Retorna
-    -------
-    str
-        Contenido del documento.
     """
 
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read()
+
+
+def load_spacy_model(language: str):
+    """
+    Carga automáticamente el modelo de spaCy
+    correspondiente al idioma detectado.
+
+    Parámetros
+    ----------
+    language : str
+        Código del idioma ('es' o 'en').
+
+    Retorna
+    -------
+    Language
+        Modelo de spaCy cargado.
+    """
+
+    if language == "es":
+        model = "es_core_news_md"
+    else:
+        model = "en_core_web_md"
+
+    return spacy.load(model)
+
+
+def process_text(text: str, nlp):
+    """
+    Procesa el documento utilizando spaCy.
+
+    Parámetros
+    ----------
+    text : str
+        Texto original.
+
+    nlp :
+        Modelo de spaCy cargado.
+
+    Retorna
+    -------
+    Doc
+        Documento procesado.
+    """
+
+    return nlp(text)
