6fbcf5ac5ddc11251842962f4301ca86b828802f
Citlalli_MT <citlallileilanimejiatorrijos@gmail.com>
2026-07-02 18:48:36 -0600

    Update answer_extractor.py

--- src/answer_extractor.py
+++ src/answer_extractor.py
@@ -8,3 +8,23 @@ Funciones:
 """
 
 from transformers import pipeline
+
+def load_qa_model():
+    """
+    Carga el modelo de Question Answering.
+
+    Retorna
+    -------
+    pipeline
+        Pipeline de extracción de respuestas.
+    """
+
+    model_name = "PlanTL-GOB-ES/roberta-base-bne-sqac"
+
+    qa_pipeline = pipeline(
+        "question-answering",
+        model=model_name,
+        tokenizer=model_name
+    )
+
+    return qa_pipeline
