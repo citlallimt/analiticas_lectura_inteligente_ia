60917f32146b02a8abc53ba2cbbbad8a34f44f84
Citlalli_MT <citlallileilanimejiatorrijos@gmail.com>
2026-07-02 18:15:36 -0600

    Update topic_model.py

--- src/topic_model.py
+++ src/topic_model.py
@@ -10,3 +10,33 @@ Funciones:
 from gensim.corpora.dictionary import Dictionary
 from gensim.models import LdaMulticore
 import yake
+
+
+def extract_tokens(doc):
+    """
+    Extrae los tokens más relevantes del documento
+    para el análisis de tópicos.
+
+    Parámetros
+    ----------
+    doc : spaCy Doc
+        Documento procesado.
+
+    Retorna
+    -------
+    list
+        Lista de tokens normalizados.
+    """
+
+    allowed_pos = {"NOUN", "PROPN", "VERB", "ADJ"}
+
+    tokens = [
+        token.lemma_.lower()
+        for token in doc
+        if token.pos_ in allowed_pos
+        and not token.is_stop
+        and not token.is_punct
+        and token.is_alpha
+    ]
+
+    return tokens
