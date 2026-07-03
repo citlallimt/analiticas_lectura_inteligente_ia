75632fdb8727b171442ee20f357ee88141502333
Citlalli_MT <citlallileilanimejiatorrijos@gmail.com>
2026-07-02 19:04:32 -0600

    Update report_generator.py

--- src/report_generator.py
+++ src/report_generator.py
@@ -9,3 +9,23 @@ Funciones:
 
 import os
 from tkinter import Tk, filedialog
+
+def select_output_directory():
+    """
+    Permite seleccionar la carpeta donde
+    se guardará el reporte final.
+
+    Retorna
+    -------
+    str
+        Ruta de la carpeta seleccionada.
+    """
+
+    root = Tk()
+    root.withdraw()
+
+    directory = filedialog.askdirectory(
+        title="Selecciona la carpeta para guardar el reporte"
+    )
+
+    return directory
